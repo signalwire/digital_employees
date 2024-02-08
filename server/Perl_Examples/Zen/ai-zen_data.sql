231a9f825372e348d9bb01c5f9ed491ad7f985d2
Len <len.pgh@gmail.com>
2024-02-08 14:51:18 -0500

    Create ai-zen_data.sql

---
+++ server/Perl_Examples/Zen/ai-zen_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers VALUES 
+(1, 'Ada', 'Lovelace', TRUE, '5551235555', '00123456789a', 4455, 8005000, '123 some st pittsburgh pa 15215', '123 some st pittsburgh pa 15215', 'ada.lovelace@livewirecalbe.com', 75.0, 955.0, 0.0, 55.0, 35, 534),
+(2, 'Alan', 'Turing', TRUE, '8675309', '00123456789b', 7856, 8005001, '123 some st pittsburgh pa 15215', '123 some st pittsburgh pa 15215', 'alan.turing@livewirecable.com', 70.0, 900.0, -9.0, 45.0, 35, 622),
+(3, 'Grace', 'Hopper', TRUE, '5551235554', '00123456789c', 4528, 8005002, '123 some st pittsburgh pa 15215', '123 some st pittsburgh pa 15215', 'grace.hopper@livewirecable.com', 5.0, 200.0, -10.0, 17.0, 35, 2),
+(4, 'John', 'Von Neumann', TRUE, '5551235553', '00123456789d', 7898, 8005003, '123 some st pittsburgh pa 15215', '123 some st pittsburgh pa 15215', 'john.vonneuman@livewirecable.com', 62.0, 890.0, -5.0, 50.0, 35, 536),
+(5, 'Blaise', 'Pascal', TRUE, '5551235552', '00123456789e', 1236, 8005004, '123 some st pittsburgh pa 15215', '123 some st pittsburgh pa 15215', 'blaise.pascal@livewirecable.com', 77.0, 999.0, 8.0, 47.0, 35, 2435667);
