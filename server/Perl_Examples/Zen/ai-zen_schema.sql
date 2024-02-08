57146f54e94c27dc667a48594b985e5c3fa65f8d
Len <len.pgh@gmail.com>
2024-02-08 14:51:57 -0500

    Create ai-zen_schema.sql

---
+++ server/Perl_Examples/Zen/ai-zen_schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE customers (
+    id INT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    Active BOOLEAN,
+    Phone_number TEXT,
+    mac_address TEXT,
+    CPNI INT,
+    Account_Number INT,
+    service_address TEXT,
+    billing_address TEXT,
+    email_address TEXT,
+    modem_speed_upload REAL,
+    modem_speed_download REAL,
+    modem_upstream_level REAL,
+    modem_downstream_level REAL,
+    modem_snr INT,
+    modem_downstream_uncorrectables INT
+);
