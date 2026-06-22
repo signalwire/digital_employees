b9bc7e3ce65576627e8ff2f7cf73423e0c66f5f3
Len Graham <len.pgh@gmail.com>
2026-06-22 17:01:37 -0400

    Update replit configs

--- server/agents_sdk_examples/bobbys_table/replit.nix
+++ server/agents_sdk_examples/bobbys_table/replit.nix
@@ -1,6 +1,6 @@
 { pkgs }: {
   # Python itself is provided by the python-3.12 module in .replit.
-  # Python packages are installed from requirements.txt by the run command
+  # Python packages are installed from requirements.txt by start_replit.sh
   # (pip). Add only SYSTEM-level deps here if a wheel ever needs one.
   deps = [];
 }
